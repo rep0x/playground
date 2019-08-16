3f5d955e35c6792b57104c0db78b22be29844ccc
Sven Reiser <svreiser@gmail.com>
2019-08-17 01:24:12 +0200

    adds basic data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import initialProfileData from './profile_data.json'
 
-function App() {
+const App = () => {
+  const [profileData, setProfileData] = useState(initialProfileData)
+  console.log(profileData)
   return (
     <div className='App'>
-      <h1>My Name Is</h1>
-      <h3>I do some stuff really good.</h3>
+      <div className='profile'>
+        <img
+          className='profile--avatar'
+          src={profileData.avatar_url}
+          alt={`${profileData.name}s profile avatar`}
+        />
+        <h1 className='profile--name'>{profileData.name}</h1>
+        <h3 className='profile--motto'>{profileData.motto}</h3>
+      </div>
     </div>
   )
 }
