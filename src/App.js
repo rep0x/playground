16dd7d9882df3db1fb414c61bff7375376a233dd
Sven Reiser <svreiser@gmail.com>
2019-08-17 01:56:05 +0200

    adds css variables, scss, edit button and form to submit new data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
-import './App.css'
+import './App.scss'
 import initialProfileData from './profile_data.json'
 
 const App = () => {
   const [profileData, setProfileData] = useState(initialProfileData)
-  console.log(profileData)
+  const [isEditable, setIsEditable] = useState(false)
+  const [name, setName] = useState(profileData.name)
+
+  const toggleEditable = () => {
+    setIsEditable(!isEditable)
+  }
+
+  const changeName = e => {
+    setName(e.target.value)
+  }
+
+  const onSubmit = () => {}
+
+  let output
+  if (isEditable) {
+    output = (
+      <form>
+        <input type='text' value={name} onChange={changeName} />
+        <button className='btn btn-primary' onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
+    )
+  }
+
   return (
     <div className='App'>
       <div className='profile'>
@@ -15,7 +39,12 @@ const App = () => {
         />
         <h1 className='profile--name'>{profileData.name}</h1>
         <h3 className='profile--motto'>{profileData.motto}</h3>
+        {/* <Skills skills={profileData.skills} /> */}
       </div>
+      {output}
+      <button className='edit btn btn-dark' onClick={toggleEditable}>
+        Edit
+      </button>
     </div>
   )
 }
